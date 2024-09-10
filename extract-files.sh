069d1b8773490db5980f4fe91ae6faea61e68599
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    pipa: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,10 @@ function blob_fixup() {
             sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_pipa\.so\x00\x00\x00\x00\x00\x00\x00|g" "${2}"
             sed -i "s|/vendor/lib/libssrec\.so|libssrec_pipa\.so\x00\x00\x00\x00\x00\x00\x00|g" "${2}"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
