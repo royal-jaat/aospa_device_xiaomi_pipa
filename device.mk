c19ba56c43fb1f2b88851a896c681d7da2cf5971
Sean Du <do4suki@gmail.com>
2024-12-07 18:33:59 +0800

    pipa: Update blobs from OS1.0.14.0.UMZMIXM
    Change-Id: I2d2456d13ad3b421a3332cbdd2e04e822b7ed7c4
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -169,8 +169,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
-    libcrypto-v33
+    android.hardware.drm-service.clearkey
 
 # Fastboot
 PRODUCT_PACKAGES += \
